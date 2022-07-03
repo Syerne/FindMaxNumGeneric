ccf962f58085260be90cd69da44275a2086cb6bf
Syerne <shubhamyerne1994@gmail.com>
2022-07-03 17:53:51 +0530

    More Parameters Passsed

--- MaxNumber.java
+++ MaxNumber.java
@@ -3,31 +3,39 @@ package com.bridgelab.generic;
 public class MaxNumber<T extends Comparable> {
 
     //instance Variables
-    T a, b, c;
+    T a, b, c, d, e;
 
     // constructor
-    public MaxNumber(T a, T b, T c) {
+
+
+    public MaxNumber(T a, T b, T c, T d, T e) {
         this.a = a;
         this.b = b;
         this.c = c;
+        this.d = d;
+        this.e = e;
     }
 
     public static void main(String[] args) {
         System.out.println("Welcome to Finding Maximum number program");
 
-        new MaxNumber<Integer>(55, 95, 59).testMaximum();
-        new MaxNumber<Float>(2.3f, 3.4f, 4.5f).testMaximum();
-        new MaxNumber<String>("Apple", "Peach", "Banana").testMaximum();
+        new MaxNumber<Integer>(55, 95, 59, 50, 60).testMaximum();
+        new MaxNumber<Float>(5.3f, 3.4f, 4.5f, 5.5f, 6.6f).testMaximum();
+        new MaxNumber<String>("Apple", "Peach", "Banana", "Mango", "Greps").testMaximum();
 
     }
 
-    public static <T extends Comparable> T findMaxNum(T a, T b, T c) {
+    public static <T extends Comparable> T findMaxNum(T a, T b, T c, T d, T e) {
         T max = a;
 
         if (b.compareTo(max) > 0)
             max = b;
         if (c.compareTo(max) > 0)
             max = c;
+        if (d.compareTo(max) > 0)
+            max = d;
+        if (e.compareTo(max) > 0)
+            max = e;
 
         System.out.println("Maximum Number : " + max);
 
@@ -35,6 +43,6 @@ public class MaxNumber<T extends Comparable> {
     }
 
     public void testMaximum() {
-        MaxNumber.findMaxNum(a, b, c);
+        MaxNumber.findMaxNum(a, b, c, d, e);
     }
 }
