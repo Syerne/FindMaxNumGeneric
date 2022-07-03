590d61481356d01a73265ed69c43cd94f51d9b70
Syerne <shubhamyerne1994@gmail.com>
2022-07-03 17:35:25 +0530

    Refactor code

--- MaxNumber.java
+++ MaxNumber.java
@@ -15,19 +15,12 @@ public class MaxNumber<T extends Comparable> {
     public static void main(String[] args) {
         System.out.println("Welcome to Finding Maximum number program");
 
-        /*MaxNumber<Integer> maxValue = new MaxNumber<Integer>(55, 95, 59);
-        purpose object creation and constructor call
-        above concept helps to below steps*/
-
         new MaxNumber<Integer>(55, 95, 59).toPrint();
         new MaxNumber<Float>(2.3f, 3.4f, 4.5f).toPrint();
         new MaxNumber<String>("Apple", "Peach", "Banana").toPrint();
 
-        //new MaxNumber<String>("Apple", "Peach", "Banana").findMaxNum("a", "c", "d");
-
     }
 
-    // generic inherit properties & compare
     public <T extends Comparable> T findMaxNum(T a, T b, T c) {
         T max = a;
 
@@ -41,9 +34,7 @@ public class MaxNumber<T extends Comparable> {
         return max;
     }
 
-    // purpose print & call compare method
-    // directly call the findMaximum in main method need to parameter again passed and no used of constructor
-    public void toPrint(){
+    public void toPrint() {
         findMaxNum(a, b, c);
     }
 }
